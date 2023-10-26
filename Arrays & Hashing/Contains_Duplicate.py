759336442f388de67a93aab2074840e5ff9cea61
MPBunce <mattb1299@gmail.com>
2023-10-25 22:19:11 -0400

    one more

--- Arrays & Hashing/Contains_Duplicate.py
+++ Arrays & Hashing/Contains_Duplicate.py
@@ -24,3 +24,17 @@ class Solution:
                 new_dict[nums[i]] = 1
                 
         return False
+
+#Another Hash Map Solution
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+    
+        dic = {}
+    
+        for i in range(len(nums)):
+            dic[ nums[i] ] = 1 + dic.get(nums[i], 0)
+            if dic[ nums[i] ] > 1:
+                return True
+            
+        return False
